39bcbe0b3507854ffb9fb20fe4599b4594a97e33
Artoym <artemaaawer@gmail.com>
2024-09-14 13:55:32 +0400

    Add files via upload

--- Cocolator/Program.cs
+++ Cocolator/Program.cs
@@ -3,11 +3,17 @@ char sign;
 
 Console.WriteLine("Добро пожаловать в калькулятор. Вам нелбходимо ввести первое число, затем знак действия(+,-,*,/), которое хотите совершить и второе число.");
 
-Console.Write("Введите первое число A:  ");
+Console.Write("Введите первое число: ");
 one = Convert.ToSingle(Console.ReadLine());
 Console.Write("Введите знак действия : ");
 sign = Convert.ToChar(Console.ReadLine());
-Console.Write("Введите Второе число B: ");
+Console.Write("Введите Второе число: ");
 two = Convert.ToSingle(Console.ReadLine());
 
-
+if (sign == '+')
+{
+    result = one + two;
+    Console.WriteLine("Сумма ваших чисел равна " + result);
+    Console.WriteLine("Для выхода нажмите любую клавишу...");
+    Console.ReadKey();
+}
