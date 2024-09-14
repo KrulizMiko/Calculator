4095363571933a466d8d9fc28423e4e455b456bb
Artoym <artemaaawer@gmail.com>
2024-09-14 13:43:48 +0400

    Add files via upload

---
+++ Cocolator/Program.cs
@@ -0,0 +1,11 @@
+﻿float one, two, result;
+char sign;
+
+Console.WriteLine("Добро пожаловать в калькулятор. Вам нелбходимо ввести первое число, затем знак действия(+,-,*,/), которое хотите совершить и второе число.");
+
+Console.Write("Введите первое число A: ");
+one = Convert.ToSingle(Console.ReadLine());
+Console.Write("Введите знак действия : ");
+sign = Convert.ToChar(Console.ReadLine());
+
+
