b6b41d4189990c5f7725786024f20e76c45813e6
Артем <168135876+KrulizMiko@users.noreply.github.com>
2024-10-07 10:57:15 +0400

    add/upFun+

--- Cocolator/Program.cs
+++ Cocolator/Program.cs
@@ -1,19 +1,69 @@
-﻿float one, two, result;
-char sign;
+﻿using System;
 
-Console.WriteLine("Добро пожаловать в калькулятор. Вам нелбходимо ввести первое число, затем знак действия(+,-,*,/), которое хотите совершить и второе число.");
-
-Console.Write("Введите первое число: ");
-one = Convert.ToSingle(Console.ReadLine());
-Console.Write("Введите знак действия : ");
-sign = Convert.ToChar(Console.ReadLine());
-Console.Write("Введите Второе число: ");
-two = Convert.ToSingle(Console.ReadLine());
-
-if (sign == '+')
+class Calculator
 {
-    result = one + two;
-    Console.WriteLine("Сумма ваших чисел равна " + result);
-    Console.WriteLine("Для выхода нажмите любую клавишу...");
-    Console.ReadKey();
+    static void Main(string[] args)
+    {
+        float one, two, result;
+        char sign;
+
+        Console.WriteLine("Добро пожаловать в калькулятор.");
+
+        while (true)
+        {
+            Console.WriteLine("Введите первое число:");
+            one = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Введите знак действия (-, *, /, +):");
+            sign = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            two = Convert.ToSingle(Console.ReadLine());
+
+            switch (sign)
+            {
+               
+                case '-':
+                    result = one - two;
+                    Console.WriteLine("Разность ваших чисел равна " + result);
+                    break;
+                case '*':
+                    result = one * two;
+                    Console.WriteLine("Произведение ваших чисел равно " + result);
+                    break;
+                case '/':
+                    if (two == 0)
+                    {
+                        Console.WriteLine("Ошибка. Делитель не может быть равным нулю.");
+                    }
+                    else
+                    {
+                        result = one / two;
+                        Console.WriteLine("Частное ваших чисел равно " + result);
+                    }
+                    break;
+                case '+':
+                    result = one + two;
+                    Console.WriteLine("Сумма ваших чисел равна " + result);
+                    break;
+
+
+
+
+                default:
+                    Console.WriteLine("Ошибка. Вы ввели неверный знак.");
+                    break;
+            }
+
+            Console.WriteLine("Хотите сделать еще один расчет? (y/n)");
+            string answer = Console.ReadLine();
+
+            if (answer.ToLower() != "y")
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("До свидания!");
+    }
 }
